fc2a4a8b1e065308ea93425590800643a2d2e730
William <sancheswillian2008@gmail.com>
2025-08-02 17:06:44 -0300

    minhas alterações no React Router e scripts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/encore-crowd/',
   server: {
     host: "::",
     port: 8080,
-    base: '/encore-crowd/',
   },
   plugins: [
     react(),
